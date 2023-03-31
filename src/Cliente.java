aad159bd254d8c631ffd58312319a9fb65e9646c
Talita Hanna Cabral <talitalinden@gmail.com>
2023-03-31 13:00:14 -0300

    Versão final - Questão 4

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class Cliente {
+public class Cliente {
+
+    private double saque;
+    private double valor_saque;
+
+    public double getSaque() {
+        return saque;
+    }
+
+    public void setSaque(double saque) {
+        this.saque = saque;
+    }
+
+    public double getDeposito() {
+        return deposito;
+    }
+
+    public void setDeposito(double deposito) {
+        this.deposito = deposito;
+    }
+
+    private double deposito;
+
+    public double getValor_saque() {
+        return valor_saque;
+    }
+
+    public void setValor_saque(double valor_saque) {
+        this.valor_saque = valor_saque;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner sc = new Scanner(System.in);
+        Cliente c1 = new Cliente();
+        System.out.println("""
+               Qual operação você deseja realizar?
+               Digite 1 para depósito.
+               Digite 2 para saque.""");
+        int escolha = sc.nextInt();
+        switch (escolha) {
+            case 1:
+                System.out.println("Qual o valor do seu depósito?");
+                c1.setDeposito(sc.nextDouble());
+                System.out.println("Depósito realizado com sucesso.");
+                break;
+            case 2:
+                System.out.println("Qual é o valor do saque?");
+                c1.setSaque(sc.nextDouble());
+                System.out.println("Saque realizado com sucesso.");
+
+                if (c1.getValor_saque() > 1000) {
+                    System.out.println("Saldo insuficiente");
+                c1.setSaque(sc.nextDouble());
+            }
+                break;
+
+        }
+
     }
 }
